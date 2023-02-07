dae54cbf50d455260da285be4d4d166cc8badb0e
Dmitriy Malakhov <mitia2022@gmail.com>
2023-02-07 17:27:06 +0700

    build issue fix

--- src/components/ShortcutsGuide.svelte
+++ src/components/ShortcutsGuide.svelte
@@ -6,10 +6,13 @@
 <h3 class="mt-2 font-mono text-lg font-medium ">Shortcuts guide</h3>
 <div class="pb-4 mt-4 space-y-3">
 	{#each shortcuts as shortcut}
-		<p class="font-mono text-sm text-gray-500">
-			<Kbd class="px-2 py-1">{shortcut.keys.join('+')}</Kbd>
+		<p class="font-mono text-sm text-gray-500 space-x-2">
+			{#each shortcut.keys as key}
+				<Kbd class="px-2 py-1">
+					{key}
+				</Kbd>
+			{/each}
 			{shortcut.title}
-			<!-- <td>{shortcut.description}</td> -->
 		</p>
 	{/each}
 </div>
