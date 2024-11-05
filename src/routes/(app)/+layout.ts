578c0e54d2df738c4130e7f5b9fa8575f5547111
Dmitrii Malakhov <mitia2022@gmail.com>
2024-11-05 16:33:11 +0100

    upd

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -4,11 +4,10 @@ import { browser } from '$app/environment';
 export const load = async () => {
 	if (browser) {
 		posthog.init('phc_wnxpq76WlBx9sJ1rsF411RfLifcb2j2pZmACjrTXGug', {
-			ui_host: 'https://eu.i.posthog.com',
-			api_host: 'https://noizer.one/ingest',
-			person_profiles: 'always',
-			capture_pageview: true,
-			capture_pageleave: true
+			api_host: 'https://eu.i.posthog.com',
+			person_profiles: 'identified_only',
+			capture_pageview: false,
+			capture_pageleave: false
 		});
 	}
 	return;
